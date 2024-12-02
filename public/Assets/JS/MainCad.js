364a696ae42e89068462a979feaedbb5c34cc949
lemos075 <lemosfelipe5164@gmail.com>
2024-12-02 14:46:48 -0300

    Initial commit

---
+++ public/Assets/JS/MainCad.js
@@ -0,0 +1,8 @@
+function redirecionarParaOutraPagina() {
+    window.location.href = "Ong.html";
+  }
+
+  document.getElementById('selectong').addEventListener('click', function() {
+    redirecionarParaOutraPagina('Ong.html');
+  });
+
